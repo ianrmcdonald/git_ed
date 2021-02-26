5cbc71384baa220b6a2d53e7436b62d56fcca9d4
Ian McDonald <ianrmcdonald@gmail.com>
2021-02-26 14:50:07 -0800

    initial commit message

---
+++ turbines.R
@@ -0,0 +1,19 @@
+
+#https://juliasilge.com/blog/wind-turbine/
+library(tidyverse)
+library(tidymodels)
+
+turbines <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-10-27/wind-turbine.csv")
+
+
+turbines_df <- turbines %>%
+     transmute(
+          turbine_capacity = turbine_rated_capacity_k_w,
+          rotor_diameter_m,
+          hub_height_m,
+          commissioning_date = parse_number(commissioning_date),
+          province_territory = fct_lump_n(province_territory, 10),
+          model = fct_lump_n(model, 10)
+     ) %>%
+     filter(!is.na(turbine_capacity)) %>%
+     mutate_if(is.character, factor)
